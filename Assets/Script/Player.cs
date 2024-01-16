75840b4cef764b024c1aacc4948787e40e565be1
mozanne <mozanne@GCFRIDF039>
2024-01-16 16:39:17 +0100

    fini

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Rendering;
@@ -10,10 +11,24 @@ public class Player : MonoBehaviour
     float verticalAxis;
     Rigidbody rb;
     [SerializeField] float speed;
-    [SerializeField] float rotationspeed;
+    [SerializeField] float rotationSpeed;
+    [SerializeField] float playerNumber;
+    [SerializeField] enum PlayerType{ player1, player2};
+    PlayerType myType;
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+
+        if(playerNumber == 1)
+        {
+            myType = PlayerType.player1;
+        }else if(playerNumber == 2)
+        {
+            myType = PlayerType.player2;
+        }
+
+        /*if (transform.rotation.y == 0)
+            transform.rotation = 0.01;*/
     }
     // Start is called before the first frame update
     void Start()
@@ -24,14 +39,26 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalAxis = Input.GetAxis("Horizontal");
+        verticalAxis = Input.GetAxis("Vertical");
         Move();
+        Rotate();
     }
 
     private void Move()
     {
-        horizontalAxis = Input.GetAxis("Horizontal");
-        verticalAxis = Input.GetAxis("Vertical");
+        if (myType == PlayerType.player1 && (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S)))
+            rb.AddForce(transform.forward * verticalAxis * speed * Time.deltaTime);
+        if (myType == PlayerType.player2 && (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow)))
+            rb.AddForce(transform.forward * verticalAxis * speed * Time.deltaTime);
+    }
 
-        rb.AddForce(new Vector3(horizontalAxis * speed * Time.deltaTime, verticalAxis * rotationspeed * Time.deltaTime), 0);
+    private void Rotate()
+    {
+        if (myType == PlayerType.player1 && (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)))
+            transform.Rotate(transform.eulerAngles, horizontalAxis * rotationSpeed * Time.deltaTime);
+        if (myType == PlayerType.player2 && (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow)))
+            transform.Rotate(transform.eulerAngles, horizontalAxis * rotationSpeed * Time.deltaTime);
     }
 }
+
